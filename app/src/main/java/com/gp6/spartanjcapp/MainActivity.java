939aea00b23a73fed33d7fbe76e9f1f83f32fa74
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2018-10-08 17:38:31 -0700

    Created Visuals of start page

--- app/src/main/java/com/gp6/spartanjcapp/MainActivity.java
+++ app/src/main/java/com/gp6/spartanjcapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.gp6.spartanjcapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configureLoginButton();
+        configureCreateAccountButton();
+    }
+
+    /**
+     * This method defines the action of the 'Log in' button.
+     * Switches activity to log in screen
+     *
+     * TODO: Check if username and password is correct
+     */
+    private void configureLoginButton(){
+        Button logInButton = (Button) findViewById(R.id.loginButton);
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HomePage.class));
+            }
+        });
+    }
+
+    /**
+     * This method defines the 'Create an account' button.
+     * Switches activity to sign up screen
+     */
+    private void configureCreateAccountButton(){
+        Button signUpButton = (Button) findViewById(R.id.createAccountButton);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUpScreen.class));
+            }
+        });
     }
 }
