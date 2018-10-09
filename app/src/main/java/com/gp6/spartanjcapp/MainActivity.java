0bdea65d9dcc3556bfceba3b9f5d6a912470afb3
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2018-10-08 17:45:18 -0700

    Updating login screen

--- app/src/main/java/com/gp6/spartanjcapp/MainActivity.java
+++ app/src/main/java/com/gp6/spartanjcapp/MainActivity.java
@@ -13,38 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        configureLoginButton();
-        configureCreateAccountButton();
-    }
-
-    /**
-     * This method defines the action of the 'Log in' button.
-     * Switches activity to log in screen
-     *
-     * TODO: Check if username and password is correct
-     */
-    private void configureLoginButton(){
-        Button logInButton = (Button) findViewById(R.id.loginButton);
-        logInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, HomePage.class));
-            }
-        });
-    }
-
-    /**
-     * This method defines the 'Create an account' button.
-     * Switches activity to sign up screen
-     */
-    private void configureCreateAccountButton(){
-        Button signUpButton = (Button) findViewById(R.id.createAccountButton);
-
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SignUpScreen.class));
-            }
-        });
     }
 }
