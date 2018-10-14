7831a937dcd41b65ee1e022bb0f33cb9434b32bf
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2018-10-14 02:02:57 -0700

    Implemented initial code for QR scan and read feature

--- app/src/main/java/com/gp6/spartanjcapp/HomePage.java
+++ app/src/main/java/com/gp6/spartanjcapp/HomePage.java
@@ -1,13 +1,64 @@
 package com.gp6.spartanjcapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class HomePage extends AppCompatActivity {
+
+    private Button qrCodeScanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        final Activity activity = this;
+        qrCodeScanButton = (Button)findViewById(R.id.qrScanButton);
+
+        qrCodeScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        //If 'result' returns null, the scan was cancelled
+        if(result.getContents() == null){
+            Toast.makeText(this, "Scan cancelled", Toast.LENGTH_LONG).show();
+        }
+        //Else get the contents of 'result'. 'result.getContents()' contains the reading of the qr code
+        else{
+
+            /*
+                pseudocode:
+
+                String decodedData[] = decodeScannedData(result.getContents()); //grabs scanned data and places all info into a nice format; stored in array
+                addNewScannedUser(decodedData); //Adds new user to home page given the decoded data
+             */
+
+
+        }
+
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
