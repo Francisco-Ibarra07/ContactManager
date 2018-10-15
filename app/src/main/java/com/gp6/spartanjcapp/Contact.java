6028aa8085216935716673cfa8513a4fbb936d54
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2018-10-14 17:34:03 -0700

    Created Contact class

---
+++ app/src/main/java/com/gp6/spartanjcapp/Contact.java
@@ -0,0 +1,39 @@
+package com.gp6.spartanjcapp;
+
+/**
+ * This class defines a "Contact" object. Each user's information, when signed up, gets stored
+ * in this object type. With this information, our ContactManager class will be able to keep a
+ * list of Contact objects and store them conveniently on Firebase
+ */
+public class Contact {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int phoneNumber;
+
+    public Contact(){
+        firstName = "";
+        lastName = "";
+        email = "";
+        phoneNumber = 0;
+    }
+
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPhoneNumber(int phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+}
+
